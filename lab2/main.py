48f83ecd80ff32282c40de8d5f982f2bd0043fb6
CosminAnd <79159337+CosminAnd@users.noreply.github.com>
2022-10-09 12:54:10 +0300

    ex3

--- lab2/main.py
+++ lab2/main.py
@@ -4,6 +4,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import arviz as az
 
+
 #ex1
 x = stats.expon.rvs(scale=1 / 4, size=1000)
 y = stats.expon.rvs(scale=1 / 6, size=1000)
@@ -39,4 +40,20 @@ for i in avg:
 print("Probability: ", favorable/10000)
 
 az.plot_posterior({"Avg:": avg})
+plt.show()
+
+# ex3
+
+#  A si B sunt  independente => p(A ^ B) = p(A) * p(B)
+# A = np.random.choice(a=['s', 'b'], p=[0.5, 0.5], size=1)
+# B = np.random.choice(a=['s', 'b'], p=[0.3, 0.7], size=1)
+# ss - 0, sb - 1, bs - 2, bb - 3
+chars = ['ss', 'sb', 'bs', 'bb']
+values = []
+for i in range(0, 100):
+    experiment = np.random.choice(a=[0, 1, 2, 3], p=[0.5 * 0.3, 0.5 * 0.7, 0.5 * 0.3, 0.5 * 0.7], size=10)
+    values.append(experiment)
+values = np.array(values)
+
+az.plot_posterior({'ss': values == 0, 'sb': values == 1, 'bs': values == 2, 'bb': values == 3})
 plt.show()
