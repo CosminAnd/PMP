b51415c590b34660b67071227f973bdaaaf8d906
CosminAnd <79159337+CosminAnd@users.noreply.github.com>
2022-10-09 12:26:15 +0300

    ex2-update

--- lab2/main.py
+++ lab2/main.py
@@ -21,4 +21,22 @@ plt.show()
 print("media este:", np.mean(q))
 print("deviatia standard:", np.std(q))
 
+
 #ex2
+# latency = stats.expon.rvs(0, 1 / 4, size=SAMPLE_SIZE_FOR_EACH_SERVER)
+
+server1 = stats.gamma.rvs(4, scale=1 / 3, size=int(10000 * 0.25)) + stats.expon.rvs(0, 1 / 4, size = int(10000 * 0.25))
+server2 = stats.gamma.rvs(4, scale=1 / 2, size=int(10000 * 0.25)) + stats.expon.rvs(0, 1 / 4, size = int(10000 * 0.25))
+server3 = stats.gamma.rvs(5, scale=1 / 2, size=int(10000 * 0.3)) + stats.expon.rvs(0, 1 / 4, size = int(10000 * 0.3))
+server4 = stats.gamma.rvs(5, scale=1 / 3, size=int(10000 * 0.2)) + stats.expon.rvs(0, 1 / 4, size = int(10000 * 0.2))
+
+avg = np.concatenate([server1, server2, server3, server4])
+favorable = 0
+for i in avg:
+    if i > 3:
+        favorable += 1
+
+print("Probability: ", favorable/10000)
+
+az.plot_posterior({"Avg:": avg})
+plt.show()
