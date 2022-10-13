48ab29992f1a64516f4809a887dec542715d65b8
CosminAnd <79159337+CosminAnd@users.noreply.github.com>
2022-10-13 17:53:49 +0300

    Bonus

--- lab3/main.py
+++ lab3/main.py
@@ -7,26 +7,29 @@ import arviz as az
 model = pm.Model()
 
 with model:
-    cutremur = pm.Bernoulli('C', 0.0005)
-    incendiu_c = pm.Deterministic('I_c', cutremur, pm.math.switch(cutremur, 0.03, 0.01))
-    incendiu = pm.Bernoulli('I', p=incendiu_c)
-    alarma_c= pm.Deterministic("A_c", pm.math.switch(cutremur, pm.math.switch(incendiu, 0.98, 0.02), pm.math.switch(incendiu, 0.95, 0.0001)))
-    alarma = pm.Bernoulli('A', p=alarma_c)
+    cutremur = pm.Bernoulli('C', 0.005)
+    incendiu_i = pm.Deterministic('I_i', pm.math.switch(cutremur, 0.005, 0.03))
+    incendiu = pm.Bernoulli('I', p=incendiu_i)
+    alarma_i = pm.Deterministic('A_i', pm.math.switch(cutremur,
+                                                  pm.math.switch(incendiu, 0.98, 0.02),
+                                                  pm.math.switch(incendiu, 0.95, 0.001)))
 
-trace = pm.sample(20000)
+    alarma = pm.Bernoulli('A', p=alarma_i)
+    trace = pm.sample(20000, chains=1)
 
-dictionary = {
-    'cutremur': trace['C'].tolist(),
-    'alarma': trace['A'].tolist(),
-    'incendiu': trace['I'].tolist()
-}
-df = pd.DataFrame(dictionary)
+    dictionary = {
+        'cutremur': trace['C'].tolist(),
+        'incendiu': trace['I'].tolist(),
+        'alarma': trace['A'].tolist()
+    }
+    df = pd.DataFrame(dictionary)
 
-p_cutremur= df[((df['cutremur']==1)& (df['alarma']==0))].shape[0]/ df[df['cutremur']==0].shape[0]
-print(p_cutremur)
+    p_cutremur = df[((df['cutremur'] == 1) & (df['alarma'] == 1))].shape[0] / df[df['cutremur'] == 1].shape[0]
+    print(p_cutremur)
 
-p_incendiu = df[((df['incendiu']==1)& (df['alarma']==0))].shape[0]/ df[df['alarma']==0].shape[0]
-print(p_incendiu)
 
-az.plot_posterior(trace)
-plt.show()
+    p_incendiu = df[((df['incendiu'] == 1) & (df['alarma'] == 0))].shape[0] / df[df['alarma'] == 0].shape[0]
+    print(p_incendiu)
+
+    az.plot_posterior(trace)
+    plt.show()
